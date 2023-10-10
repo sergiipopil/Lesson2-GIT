4f283f56929d0283d1ae2fad38b0d909feeecf7a
Sergii <73257275+sergiipopil@users.noreply.github.com>
2023-10-10 13:50:38 +0300

    Test (#1)
    * commit3
    
    * commit4

--- Program.cs
+++ Program.cs
@@ -1,7 +1,10 @@
 ﻿using Lesson2_GIT.Classes;
 
+global::System.Console.WriteLine();
 var sum = new Calculator(2, 3).Sum();
 var sub = new Calculator(12, 3).Subtraction();
-
+global::System.Console.WriteLine();
 Console.WriteLine($"2+3={sum}");
 Console.WriteLine($"12-3={sub}");
+global::System.Console.WriteLine();
+global::System.Console.WriteLine();
