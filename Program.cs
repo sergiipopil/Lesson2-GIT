3952f8ad2a77654f42a216e66872e14ff0a8b0b8
SergiiPopil <sergii.popil@gmail.com>
2023-10-10 14:03:55 +0300

    squash 2 comits

--- Program.cs
+++ Program.cs
@@ -1,9 +1,6 @@
 ﻿using Lesson2_GIT.Classes;
 
-global::System.Console.WriteLine();
 var sum = new Calculator(2, 3).Sum();
 var sub = new Calculator(12, 3).Subtraction();
-global::System.Console.WriteLine();
 Console.WriteLine($"2+3={sum}");
 Console.WriteLine($"12-3={sub}");
-global::System.Console.WriteLine();
