ff2c572198a2ed80d0f0eb5cfbd016a6374bd556
Serhii Popil <sergii.popil@gmail.com>
2023-10-10 11:43:43 +0300

    commit_2

--- Program.cs
+++ Program.cs
@@ -4,5 +4,6 @@ var sum = new Calculator(2, 3).Sum();
 var sub = new Calculator(12, 3).Subtraction();
 
 Console.WriteLine("Start program");
+Console.WriteLine("Calculation Example:\n");
 Console.WriteLine($"2+3={sum}");
 Console.WriteLine($"12-3={sub}");
