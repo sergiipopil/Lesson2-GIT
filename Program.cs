d178db8d3009bbba6e47552e2eae48366438a0f6
SergiiPopil <sergii.popil@gmail.com>
2023-10-10 13:58:41 +0300

    qwerty4

--- Program.cs
+++ Program.cs
@@ -2,6 +2,5 @@
 
 var sum = new Calculator(2, 3).Sum();
 var sub = new Calculator(12, 3).Subtraction();
-global::System.Console.WriteLine();
 Console.WriteLine($"2+3={sum}");
 Console.WriteLine($"12-3={sub}");
